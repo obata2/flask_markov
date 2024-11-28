cb0d3cf91e5dc2a6c786163e4f4d3c68d2a0f3ce
obata2 <c223062@ed.nagoya-cu.ac.jp>
2024-11-28 22:03:21 +0900

    add : MeCabの導入

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 from flask import Flask
+import MeCab
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+  mecab = MeCab.Tagger()
+  text = "私は日本語を勉強しています。"
+  nodes = mecab.parseToNode(text)
+  tokens = []
+  while nodes:
+    if nodes.surface != "":
+      tokens.append(nodes.surface)
+      nodes = nodes.next
+  return tokens
