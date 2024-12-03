88d65ffe95a51788006ce8a3c86e04fe3e555785
obata2 <c223062@ed.nagoya-cu.ac.jp>
2024-12-03 12:09:14 +0900

    add : index.htmlに、全選択と入力クリアを追加

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def make_sentence(model, sentence_num, seed="[BOS]", max_words = 1000):
     queue = deque(list(markov_key), len(list(model.keys())[0]))
     sentence = "".join(markov_key)
     for _ in range(max_words):
-        markov_key = tuple(queue)           #1文にある程度単語数がないと、"。"には繋げられないようにするか...？
+        markov_key = tuple(queue)
         next_word = random.choice(model[markov_key])
         sentence += next_word
         queue.append(next_word)
@@ -65,17 +65,18 @@ def make_sentence(model, sentence_num, seed="[BOS]", max_words = 1000):
     result = sentence.replace(seed, "")
     return result
 
-#適切なファイルパスを指定し、テキストデータを返す           current_dir\texts\(どのお話か)\(stepは何か).txt  を戻り値に
-def get_text(current_dir, storytype_i, step):
-  path = os.path.join(current_dir, "texts", storytype_i, step)
+#適切なファイルパスを指定し、テキストデータを返す
+def get_text(current_dir, text_i, step):
+  path = os.path.join(current_dir, "texts", text_i, step)
   with open(path, "r", encoding="utf-8") as file:
     return file.read()
   
 #stepに応じて、"テキストデータの取得→文章生成→return" の一連の流れを行う
-def get_result(current_dir, storytype_1, storytype_2, step, order, sentence_num):
-  text_1 = get_text(current_dir, storytype_1, step)
-  text_2 = get_text(current_dir, storytype_2, step)
-  text_sum = text_1 + text_2
+def get_result(selected_options, step, order, sentence_num):
+  text_sum = ""
+  current_dir = os.path.dirname(__file__)
+  for text_i in selected_options:
+    text_sum += get_text(current_dir, text_i, step)
   model = make_model(text_sum, order)
   return make_sentence(model, sentence_num)
 
@@ -83,27 +84,24 @@ def get_result(current_dir, storytype_1, storytype_2, step, order, sentence_num)
 @app.route("/view_result", methods = ["POST"])
 def view_result():
   selected_options = request.form.getlist('options')
-  current_dir = os.path.dirname(__file__)
-  storytype_1 = "issunbousi"
-  storytype_2 = "hanasaka"
   result = ""
   #beginningについての文章生成
   step = "beginning.txt"
   order = 1
   sentence_num = 1
-  result += get_result(current_dir, storytype_1, storytype_2, step, order, sentence_num)
+  result += get_result(selected_options, step, order, sentence_num)
   result += "(@-@)"
   #middleについての文章生成
   step = "middle.txt"
   order = 2
   sentence_num = 20
-  result += get_result(current_dir, storytype_1, storytype_2, step, order, sentence_num)
+  result += get_result(selected_options, step, order, sentence_num)
   result += "(@-@)"
   #endについての文章生成
   step = "end.txt"
   order = 1
   sentence_num = 1
-  result += get_result(current_dir, storytype_1, storytype_2, step, order, sentence_num)
+  result += get_result(selected_options, step, order, sentence_num)
   result += "(@-@)おしまい。"
   return result
 
